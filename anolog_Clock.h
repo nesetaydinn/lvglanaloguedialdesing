3bf0267c52243759d9c31884becadeb078f56e01
Neşet Aydın <50285326+nesetaydinn@users.noreply.github.com>
2020-01-19 17:59:36 +0300

    Update anolog_Clock.h

--- anolog_Clock.h
+++ anolog_Clock.h
@@ -17,37 +17,17 @@ extern "C" {
 #include "../../lvgl/lvgl.h"
 #include "../../lv_ex_conf.h"
 #endif
+#include "tosbaga.h"
+#if TOS_TEST
 
-
-struct timeRadian
-{
-	double nanoSecondsRadian;
-	double secondsRadian;
-	double minutesRadian;
-	double hoursRadian;
-};
-/*Ana Fonksiyonlarý*/
+/*Ana Fonksiyonlari*/
 void analog_Clock(void);
-static void ekran_Yarat(void);
-static void init_all_themes(uint16_t hue); //Tema Seçimi
-/*Saat Fonksiyonlarý*/
-void clockCanvas(void);
-void analogClock_animation(lv_obj_t * canvas, int16_t value);
-void drawStatic(lv_obj_t * analogSaat);
-void drawDinamic(lv_obj_t * analogSaat);
-/*Hesap Fonksiyonlarý*/
-float mapValue(float deger,int start1,int stop1,int start2,float stop2);
-float normalize(int deger,int start,int stop);
-void my_getTime(int *saniye, int *dakika, int *saat);
-void my_setTime(int  saat, int  dakika, int  saniye);
+void tos_ex_analogclktest(void);
 
-int my_seconds;
-int my_minute;
-int my_hours;
 
-double nanoSaniyeRadyan, saniyeRadyan, dakikaRadyan, saatRadyan;
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
 
+#endif
 #endif
